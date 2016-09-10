b36e3192d337b126e062209f596cb6f0c70bae4d
Aasha Medhi <aasha.medhi@inmobi.com>
2016-09-10 11:59:55 +0530

    Added course task manager

---
+++ ChildConnect/app/src/main/java/com/acubeapps/childconnect/task/ProblemActivity.java
@@ -0,0 +1,15 @@
+package com.acubeapps.childconnect.task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.acubeapps.childconnect.R;
+
+public class ProblemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problem);
+    }
+}
