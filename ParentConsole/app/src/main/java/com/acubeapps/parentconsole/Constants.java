1a07c5cdaba4e430e7b492e41da42aba61ed7832
Aasha Medhi <aasha.medhi@inmobi.com>
2016-09-11 01:08:42 +0530

    Added sign in for parent

--- ParentConsole/app/src/main/java/com/acubeapps/parentconsole/Constants.java
+++ ParentConsole/app/src/main/java/com/acubeapps/parentconsole/Constants.java
@@ -4,19 +4,12 @@ package com.acubeapps.parentconsole;
  * Created by aasha.medhi on 9/10/16.
  */
 public class Constants {
-    public static final String LOG_TAG = "ChildConnect: ";
+    public static final String LOG_TAG = "ParentConsole: ";
 
-    public static final String ERROR_INVALID_PARENT_ID = "Please add a valid parent id";
     public static final String SUCCESS = "SUCCESS";
     public static final String FAILURE = "FAILURE";
     public static final String COURSE_ID = "COURSE_ID";
     public static final String PACKAGE_NAME = "PACKAGE_NAME";
     public static final String QUESTION_ID = "QUESTION_ID";
-    public static final String SOURCE_GOOGLE = "Google";
-    public static final String CHILD_ID = "CHILD_ID";
 
-    public static final String LAST_UPLOAD_STATUS = "upload.status";
-    public static final String LAST_SUCCESSFUL_UPLOAD_TIME = "last.successful.upload.time";
-    public static final String JOB_START_TIME = "job.start.time";
-    public static final String JOB_END_TIME = "job.end.time";
 }
