c355359f43b0f2b1dd06e0dca4ab0355c6bad333
Aasha Medhi <aasha.medhi@inmobi.com>
2016-09-11 12:10:42 +0530

    Added parent id

--- ParentConsole/app/src/main/java/com/acubeapps/parentconsole/ListChildActivity.java
+++ ParentConsole/app/src/main/java/com/acubeapps/parentconsole/ListChildActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import com.acubeapps.parentconsole.adapters.ChildListAdapter;
 import com.acubeapps.parentconsole.model.GetChildListResponse;
@@ -29,6 +30,9 @@ public class ListChildActivity extends AppCompatActivity {
     @BindView(R.id.recycler_view_child_list)
     RecyclerView childRecyclerView;
 
+    @BindView(R.id.txt_parentId)
+    TextView txtParentId;
+
     ChildListAdapter childListAdapter;
 
     @Override
@@ -39,6 +43,7 @@ public class ListChildActivity extends AppCompatActivity {
         Injectors.appComponent().injectListChildActivity(this);
         ButterKnife.bind(this);
         String parentId = sharedPreferences.getString(Constants.PARENT_ID, null);
+        txtParentId.setText("Your id is : "+ parentId);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         childRecyclerView.setLayoutManager(mLayoutManager);
         childRecyclerView.setItemAnimator(new DefaultItemAnimator());
